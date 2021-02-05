ef11d649945ba9bcce05b0e482e6668648b81e83
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-02-05 01:14:06 -0500

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,34 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      isToggle: false
+    }
+  }
+
+  handleToggle = () => {
+    this.setState({
+      isToggle: !this.state.isToggle
+    })
+  }
+
+
   render() {
     return (
       <Card>
         <div>
-          <div className="image">
-            <img alt="oh no!" />
+          <div className="image" onClick={this.handleToggle}>
+            <img src = {this.state.isToggle ? this.props.pokemon.sprites.back : this.props.pokemon.sprites.front} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.props.pokemon.hp}
             </span>
           </div>
         </div>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -4,20 +4,78 @@ import PokemonForm from './PokemonForm'
 import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
+let URL = "http://localhost:3001/pokemon"
+let POKEMON =[]
+
 class PokemonPage extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      pokemons: []
+    }
+  }
+
+
+  componentDidMount() {
+    fetch(URL)
+    .then(res=> res.json())
+    .then(pokemonData => {
+      POKEMON = pokemonData
+      this.setState({
+        pokemons: pokemonData
+      })
+    })
+  }
+
+
+  handleChange = (event) => {
+    event.preventDefault()
+    this.setState({
+      pokemons: POKEMON.filter(pokemon => pokemon.name.includes(event.target.value))
+    })
+  }
+
+
+  handleAddPokemon = (event) => {
+    let newPokemon = {
+      name: event.target.name.value,
+      hp: event.target.hp.value,
+      sprites: {
+        front: event.target.frontUrl.value,
+        back: event.target.backUrl.value
+      }
+    }
+
+    let reqPackage = {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(newPokemon)
+    }
+
+    fetch(URL, reqPackage)
+    .then(res => res.json())
+    .then(newPokemon => {
+      this.setState({
+        pokemons: [...this.state.pokemons, newPokemon]
+      })
+    })
+    event.target.reset()
+
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleAddPokemon={this.handleAddPokemon}/>
         <br />
-        <Search />
+        <Search handleChange = {this.handleChange}/>
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemonData={this.state.pokemons}/>
       </Container>
     )
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
